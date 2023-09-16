bb3ba76235971d58e8fedbe703b8d735df05078e
Jae <gideonoakinduro@gmail.com>
2023-09-16 07:41:25 +0100

    aws/cert resolve attempt0

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
       { protocol: "https", hostname: "firebasestorage.googleapis.com" },
     ],
   },
+  resolve: {
+    alias: {
+      "aws-crt": path.resolve(__dirname, "node_modules/aws-crt"),
+    },
+  },
 };
 
 module.exports = nextConfig;
